54e5c5a8721056eabd989511d1bf4ec470641e0d
Ahmed ElSayed <ahmels@microsoft.com>
2013-10-09 15:12:42 -0700

    first

---
+++ Preview_Mvc5/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace Preview_Mvc5
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
